60e5016109d139f7dd71d57e55104ca2f4f7eb6e
Ylearven <ylegood@gmail.com>
2024-04-24 13:28:56 +0100

    update

--- main.js
+++ main.js
@@ -1,23 +1,20 @@
-import './style.css'
 //Carta de productos
-const products = [
+const productos = [
   {
     name: 'SHEIN WYWH Top De Cuello Halter',
     price: 5.1,
     stars: 5,
     reviews: 500,
     seller: 'SHEIN WYWH',
-    image:
-      'https://es.shein.com/SHEIN-WYWH-Ladies-Solid-Texture-Halterneck-Top-p-28765755.html?src_identifier=on%3DIMAGE_COMPONENT%60cn%3Dshopbycate%60hz%3DhotZone_2%60ps%3D4_2%60jc%3DitemPicking_008255621&src_module=All&src_tab_page_id=page_home1713785801766&mallCode=1&pageListType=2&imgRatio=3-4g'
+    image: './assets/ropa_1.jpg'
   },
   {
     name: 'SHEIN MOD Top de Punto',
     price: 5,
-    stars: 5,
+    stars: 4,
     reviews: 1000,
     seller: 'SHEIN MOD',
-    image:
-      'https://es.shein.com/SHEIN-MOD-Women-S-Solid-Color-Pleated-Knitted-Top-p-28809068.html?src_identifier=on%3DIMAGE_COMPONENT%60cn%3Dshopbycate%60hz%3DhotZone_2%60ps%3D4_2%60jc%3DitemPicking_008255621&src_module=All&src_tab_page_id=page_home1713785801766&mallCode=1&pageList'
+    image: './assets/ropa_2.jpg'
   },
   {
     name: 'SHEIN Essnce Camiseta unicolor',
@@ -25,8 +22,7 @@ const products = [
     stars: 5,
     reviews: 1000,
     seller: 'SHEIN Essnce',
-    image:
-      'https://es.shein.com/SHEIN-Essnce-Solid-Round-Neck-Tee-p-15968402.html?src_identifier=on%3DIMAGE_COMPONENT%60cn%3Dshopbycate%60hz%3DhotZone_2%60ps%3D4_2%60jc%3DitemPicking_008255621&src_module=All&src_tab_page_id=page_home1713785801766&mallCode=1&pageListType=2'
+    image: './assets/Ropa_3.jpg'
   },
   {
     name: 'SHEIN Frenchy Camiseta Casual',
@@ -34,17 +30,15 @@ const products = [
     stars: 5,
     reviews: 1000,
     seller: 'SHEIN Fenchy',
-    image:
-      'https://es.shein.com/SHEIN-Frenchy-Solid-Color-Short-Sleeve-Casual-T-Shirt-p-27164927.html?src_identifier=on%3DIMAGE_COMPONENT%60cn%3Dshopbycate%60hz%3DhotZone_2%60ps%3D4_2%60jc%3DitemPicking_008255621&src_module=All&src_tab_page_id=page_home1713785801766&mallCode=1&pageListType=2&imgRatio=3-4'
+    image: './assets/Ropa_4.jpg'
   },
   {
     name: 'SHEIN MOD',
     price: 5,
-    stars: 5,
+    stars: 3,
     reviews: 500,
     seller: 'SHEIN MOD',
-    image:
-      'https://es.shein.com/SHEIN-MOD-Square-Neck-Ruched-Red-Slim-Fit-Tank-Top-p-21006440.html?src_identifier=on%3DIMAGE_COMPONENT%60cn%3Dshopbycate%60hz%3DhotZone_2%60ps%3D4_2%60jc%3DitemPicking_008255621&src_module=All&src_tab_page_id=page_home1713785801766&mallCode=1&pageListType=2&imgRatio=3-4'
+    image: './assets/Ropa_5.jpg'
   },
   {
     name: 'SHEIN WYWH Top Boho',
@@ -52,26 +46,23 @@ const products = [
     stars: 5,
     reviews: 500,
     seller: 'SHEIN WYWH',
-    image:
-      'https://es.shein.com/SHEIN-WYWH-WYWH-Vacation-Blue-White-Paisley-Print-Self-Tie-Halter-Boho-Top-p-10900594.html?src_identifier=on%3DIMAGE_COMPONENT%60cn%3Dshopbycate%60hz%3DhotZone_2%60ps%3D4_2%60jc%3DitemPicking_008255621&src_module=All&src_tab_page_id=page_home1713785801766&mallCode=1&pageListType=2'
+    image: './assets/ropa_1.jpg'
   },
   {
     name: 'SHEIN LUNE Camiseta',
     price: 9,
-    stars: 5,
+    stars: 4,
     reviews: 500,
     seller: 'SHEIN LUNE',
-    image:
-      'https://es.shein.com/SHEIN-LUNE-Plus-Size-Women-s-Summer-Casual-Solid-Color-T-Shirt-With-Ruffled-Sleeves-And-Round-Neckline-p-32050319.html?src_identifier=on%3DIMAGE_COMPONENT%60cn%3Dshopbycate%60hz%3DhotZone_2%60ps%3D4_2%60jc%3DitemPicking_008255621&src_module=All&src_tab_page_id=page_home1713785801766&mallCode=1&pageListType=2'
+    image: './assets/ropa_2.jpg'
   },
   {
     name: 'SHEIN LUNE Camiseta De Manga Corta',
     price: 5.95,
-    stars: 5,
+    stars: 2,
     reviews: 500,
     seller: 'SHEIN LUNE',
-    image:
-      'https://es.shein.com/SHEIN-LUNE-Plus-Size-Women-s-V-Neck-Bat-Short-Sleeved-T-Shirt-p-28328049.html?src_identifier=on%3DIMAGE_COMPONENT%60cn%3Dshopbycate%60hz%3DhotZone_2%60ps%3D4_2%60jc%3DitemPicking_008255621&src_module=All&src_tab_page_id=page_home1713785801766&mallCode=1&pageListType=2'
+    image: './assets/Ropa_3.jpg'
   },
   {
     name: 'SHEIN Essnce Top De Tirante',
@@ -79,34 +70,67 @@ const products = [
     stars: 5,
     reviews: 500,
     seller: 'SHEIN Essnce',
-    image:
-      'https://es.shein.com/SHEIN-Essnce-Contrast-Binding-Cami-Top-p-20774741.html?src_identifier=on%3DIMAGE_COMPONENT%60cn%3Dshopbycate%60hz%3DhotZone_2%60ps%3D4_2%60jc%3DitemPicking_008255621&src_module=All&src_tab_page_id=page_home1713785801766&mallCode=1&pageListType=2'
+    image: './assets/Ropa_4.jpg'
   },
   {
     name: 'SHEIN Essnce Camiseta Talla Grande',
     price: 7,
-    stars: 5,
+    stars: 1,
     reviews: 500,
     seller: 'SHEIN Essnce',
-    image:
-      'https://es.shein.com/SHEIN-Essnce-Plus-Cold-Shoulder-Batwing-Sleeve-Tee-p-18743160.html?src_identifier=on%3DIMAGE_COMPONENT%60cn%3Dshopbycate%60hz%3DhotZone_2%60ps%3D4_2%60jc%3DitemPicking_008255621&src_module=All&src_tab_page_id=page_home1713785801766&mallCode=1&pageListType=2'
+    image: './assets/Ropa_5.jpg'
   }
 ]
-const divRopa = document.querySelector('#carta_ropa')
+
+const printRopa = (ropa) => {
+  const divRopa = document.querySelector('#carta')
+  divRopa.innerHTML = '' //Esto se usa para vaciar los datos y que no se dupliquen los datos cada vez que se ejecuta
+
+  for (const camisetas of ropa) {
+    //Esto es para crear los cuadraditos de la ropa
+    const divCamisetas = document.createElement('div')
+    const divImagen = document.createElement('div')
+    const imagen = document.createElement('img')
+    const nombre = document.createElement('h3')
+    const precio = document.createElement('p')
+    const divEstrellas = document.createElement('div')
+
+    //bucle para crear las estrellas
+    for (let i = 1; i <= 5; i++) {
+      const estrella = document.createElement('div')
+      estrella.className = 'estrella'
+      divEstrellas.appendChild(estrella)
+      if (camisetas.stars >= i) {
+        estrella.classList.add('rellena')
+      }
+    }
+    //Poner los contenidos
+    divCamisetas.className = 'flex-container'
+    divImagen.className = 'imgContainer'
+    imagen.src = camisetas.image
+    precio.textContent = camisetas.price
+    nombre.textContent = camisetas.name
+    //Se añaden las cosas
+    divCamisetas.appendChild(divImagen)
+    divImagen.appendChild(imagen)
+    divCamisetas.appendChild(nombre)
+    divCamisetas.appendChild(precio)
+    divCamisetas.appendChild(divEstrellas)
+    divRopa.appendChild(divCamisetas)
+  }
+}
+printRopa(productos)
+
+/* const divRopa = document.querySelector('#cartaropa')
 products.forEach((ropa) => {
   const divropa = document.createElement('div')
   divropa.className = 'camisetas'
 
   divropa.appendChild(divCamiseta)
 })
-
+ */
 /* const seccionRopa = document.querySelector('carta_ropa')*/
-for (const ropa of ropa) {
-  const imagen = document.createElement('img')
-  const nombre = document.createElement('h2')
-  const estrella = document.createElement('img')
-  const precio = document.createElement('h3')
-}
+
 /*imagen.src = products.image
 nombre.src = products.name
 estrella.src = products.stars
